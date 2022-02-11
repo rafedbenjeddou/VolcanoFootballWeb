50fc13d761c8da3e9a01af5cbdf8958b9427fed3
Khairi Slimani <61380055+KhairiSlimani@users.noreply.github.com>
2022-02-11 23:13:05 +0100

    Afficher Utilisateur

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,4 +18,16 @@ class UtilisateurController extends AbstractController
             'controller_name' => 'UtilisateurController',
         ]);
     }
+
+    /**
+     * @param UtilisateurRepository $repository
+     * @return Response
+     * @route("/AfficherUtilisateur", name="AfficheUtilisateur")
+     */
+    public function Affiche(UtilisateurRepository $repository){
+        $utilisateur=$repository->findAll();
+        return $this->render('utilisateur/afficher.html.twig',
+            ['utilisateur'=>$utilisateur]);
+    }
+
 }
