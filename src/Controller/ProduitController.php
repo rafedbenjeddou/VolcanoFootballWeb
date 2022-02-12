ba1e91189370c75c187b3fd761e637d3730a9b75
Khairi Slimani <61380055+KhairiSlimani@users.noreply.github.com>
2022-02-12 20:37:33 +0100

    Afficher Produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,4 +18,16 @@ class ProduitController extends AbstractController
             'controller_name' => 'ProduitController',
         ]);
     }
+
+    /**
+     * @param ProduitRepository $repository
+     * @return Response
+     * @route("/AfficherProduit", name="AfficherProduit")
+     */
+    public function Affiche(ProduitRepository $repository){
+        $produit=$repository->findAll();
+        return $this->render('produit/afficher.html.twig',
+            ['produit'=>$produit]);
+    }
+
 }
